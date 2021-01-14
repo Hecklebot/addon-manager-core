eb388d8f562c1868cfe30280807f0f6964b8c37f
hecklebot <iuehakdrk@gmail.com>
2021-01-14 22:02:49 +0900

    initial commit

---
+++ src/test/java/com/hecklebot/addonmanager/AddonmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecklebot.addonmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddonmanagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
